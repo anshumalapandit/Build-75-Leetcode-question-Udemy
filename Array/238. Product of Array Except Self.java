3783af299b6057263f74830af4eb1a218747706c
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-11-23 14:48:34 +0530

    Update 238. Product of Array Except Self.java

--- Array/238. Product of Array Except Self.java
+++ Array/238. Product of Array Except Self.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Time limit exceeded 
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
         int ans[]=new int[n];
@@ -14,3 +15,29 @@ class Solution {
         return ans;
     }
 }
+// Better Approach T.C=>0(N) S.C=> 0(3N)=0(N) without using division operator
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int ans[]=new int[n];
+        int pp[]=new int[n];
+        pp[0]=1; // in left we have nothing
+        int sp[]=new int[n];
+        sp[n-1]=1; // in right we have nothing
+        // calculating prefix profuct
+        for(int i=1;i<n;i++){
+            pp[i]=pp[i-1]*nums[i-1];
+        }
+        
+        //calculating suffix product
+        for(int i=n-2;i>=0;i--){
+            sp[i]=sp[i+1]*nums[i+1];
+        }
+        // now multiplying corresponding value and storing in ans array
+        for(int i=0;i<n;i++){
+            int curr=pp[i]*sp[i];
+            ans[i]=curr;
+        }
+        return ans;
+    }
+    }
